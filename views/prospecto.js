9d30d5defe332a790072963661e2068375843f7d
Chema <Chemardo3@gmail.com>
2021-04-22 14:27:10 -0600

    se elimina multer

--- views/prospecto.js
+++ views/prospecto.js
@@ -3,21 +3,6 @@ var router = express.Router();
 
 var prospectosController = require('../controllers/prospecto');
 
-
-const multer = require("multer");
-
-var storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, "./public/files");
-  },
-  filename: function(req, file, cb) {
-    cb(null, file.originalname);
-  }
-});
-  
-var uploadDisk = multer({ storage: storage })
-
-
 router.get('/prospecto', async (req, res) => {
   var resultado = await prospectosController.obtenerProspecto(req.query);
   await res.send(resultado);
